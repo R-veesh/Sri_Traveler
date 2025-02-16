34fc65c41082d7b746e9a19756ab6f2ebd274e8d
Rashking <raweeshalakruvan5@gmail.com>
2025-02-16 20:49:39 +0530

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,42 @@
+// import 'package:flutter/material.dart';
+// import 'login.dart';
 
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: loginPage(),
+//     );
+//   }
+// }
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const loginPage(),
+    );
+  }
+}
