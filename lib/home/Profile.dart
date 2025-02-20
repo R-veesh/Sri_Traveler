203a4112276d479b7ca08f7b2becfc3ded5597a3
Rashking <raweeshalakruvan5@gmail.com>
2025-02-20 23:34:56 +0530

    Add social media links to user profile

--- lib/home/Profile.dart
+++ lib/home/Profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:sri_traveler/home/profile/appBar_widget.dart';
 import 'package:sri_traveler/home/profile/button_widget.dart';
 import 'package:sri_traveler/home/profile/profile_widget.dart';
@@ -34,12 +35,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
           const SizedBox(height: 24),
           buildBioUser(User),
+          const SizedBox(height: 24),
+          buildSocialMediaLinks(),
         ],
       ),
     );
   }
 
-  buildName(User user) => Column(
+  Widget buildName(User user) => Column(
         children: [
           Text(
             user.name,
@@ -53,11 +56,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ],
       );
 
-  buildUpgradeButton() => ButtonWidget(
+  Widget buildUpgradeButton() => ButtonWidget(
         text: 'Edit the profile',
         onClicked: () {},
       );
-  buildBioUser(User user) => Container(
+  Widget buildSocialMediaLinks() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            icon: Icon(LineIcons.facebook),
+            color: Colors.blue,
+            iconSize: 40,
+            onPressed: () {
+              // Add your Facebook link here
+            },
+          ),
+          const SizedBox(width: 20),
+          IconButton(
+            icon: Icon(LineIcons.twitter),
+            color: Colors.lightBlue,
+            iconSize: 40,
+            onPressed: () {
+              // Add your Twitter link here
+            },
+          ),
+          const SizedBox(width: 20),
+          IconButton(
+            icon: Icon(LineIcons.instagram),
+            color: Colors.purple,
+            iconSize: 40,
+            onPressed: () {
+              // Add your Instagram link here
+            },
+          ),
+        ],
+      );
+
+  Widget buildBioUser(User user) => Container(
         padding: EdgeInsets.symmetric(horizontal: 48),
         child: Column(
           children: [
