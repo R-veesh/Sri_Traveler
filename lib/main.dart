c0e4290a2bfccc62842495541ce7c8a40f750076
Rashking <raweeshalakruvan5@gmail.com>
2025-02-24 03:37:06 +0530

    home page add crad and get data

--- lib/main.dart
+++ lib/main.dart
@@ -23,28 +23,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-// import 'package:device_preview/device_preview.dart';
-// import 'package:flutter/material.dart';
-// import 'login.dart';
-
-// void main() => runApp(
-//       DevicePreview(
-//         builder: (context) => MyApp(), // Wrap your app
-//       ),
-//     );
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       useInheritedMediaQuery: true,
-//       locale: DevicePreview.locale(context),
-//       builder: DevicePreview.appBuilder,
-//       theme: ThemeData.light(),
-//       darkTheme: ThemeData.dark(),
-//       home: const loginPage(),
-//     );
-//   }
-// }
